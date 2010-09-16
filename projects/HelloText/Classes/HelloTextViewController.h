ac5f5b8cb03c48686ca2caa8077c3a8c1900d2c0
Jason Harwig <jason.harwig@nearinfinity.com>
2010-09-16 14:38:58 -0400

    added memory management notes

--- projects/HelloText/Classes/HelloTextViewController.h
+++ projects/HelloText/Classes/HelloTextViewController.h
@@ -5,9 +5,12 @@
 @interface HelloTextViewController : UIViewController<UITextFieldDelegate> {
     IBOutlet UILabel *label;
     IBOutlet UITextField *textField;
+    
+    NSArray *savedText;
 }
 
 - (IBAction)sayHello:(id)sender;
+- (IBAction)savedText:(id)sender;
 
 @end
 
--- projects/HelloText/Classes/HelloTextViewController.m
+++ projects/HelloText/Classes/HelloTextViewController.m
@@ -5,6 +5,8 @@
 
 // Change some of the interface before showing to screen
 - (void)viewDidLoad {
+    savedText = [[NSArray alloc] init];
+    
     // Clear label text
     label.text = nil;
     
@@ -21,9 +23,23 @@
 
 // Method for button action
 - (IBAction)sayHello:(id)sender {
+
+    // broken because savedText will be autoreleased sometime later, then crash later when accessed
+    //savedText = [savedText arrayByAddingObject:textField.text];
+    
+    // fixed
+    savedText = [[savedText arrayByAddingObject:textField.text] retain];
+    
     label.text = [NSString stringWithFormat:@"Hello %@", textField.text];
     textField.text = nil;
     [textField resignFirstResponder];
+}
+
+- (IBAction)savedText:(id)sender
+{
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Saved text" message:[savedText componentsJoinedByString:@","] delegate:nil cancelButtonTitle:nil otherButtonTitles:@"OK", nil];
+    [alert show];
+    [alert release];
 }
 
 // Delegate method for UITextField when return is pressed
@@ -37,6 +53,7 @@
 - (void)dealloc {
     [label release];
     [textField release];
+    [savedText release];
     [super dealloc];
 }
 
